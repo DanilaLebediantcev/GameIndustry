0629445d5d888cca331035dfbc8d97e20f9a6240
Danila Lebediantcev <lebedyantsevds@yandex.ru>
2020-05-04 13:57:02 +0300

    add some tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,14 @@
 import Reposiroty.CompanyRepository;
 import Reposiroty.GameRepository;
+import Reposiroty.GenreRepository;
 import Reposiroty.PersonRepository;
 import db.DBConnection;
 import db.HibernateConnection;
 import entity.Company;
 import entity.Game;
+import entity.Genre;
 import entity.Person;
-import org.hibernate.Session;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,88 +32,18 @@ public class Main {
         Game fifa = new Game("FIFA2020");
         Game battlefield = new Game("BattlefieldV");
 
+        //Create genre
+        Genre mmo = new Genre("MMO");
+        Genre rpg = new Genre("RPG");
 
         //Repositories
         CompanyRepository companyRepository = new CompanyRepository();
         GameRepository gameRepository = new GameRepository();
         PersonRepository personRepository = new PersonRepository();
+        GenreRepository genreRepository = new GenreRepository();
 
 
 
-        ubisoft.addGameToGamesList(assassins);
-        ubisoft.addGameToGamesList(anno);
-
-        companyRepository.addCompany(ubisoft);
-
-
-//        companyRepository.addGameToGamesList(assassins);
-//        companyRepository.addGameToGamesList(anno);
-
-
-
-        System.out.println("/--------------------------------------------------/");
-        System.out.println("All companies");
-        companyRepository.getAllCompany().forEach(company -> System.out.println(company.toString()));
-
-        System.out.println("All games");
-        gameRepository.getAllGames().forEach(game -> System.out.println(game.toString()));
-
-        ubisoft.setName("UBIIIIIIIIIII");
-        companyRepository.updateCompany(ubisoft);
-
-        System.out.println("/--------------------------------------------------/");
-        System.out.println("All companies");
-        companyRepository.getAllCompany().forEach(company -> System.out.println(company.toString()));
-
-        System.out.println("All games");
-        gameRepository.getAllGames().forEach(game -> System.out.println(game.toString()));
-        System.out.println("/--------------------------------------------------/");
-        System.out.println("Change game's name , assassins -> LALALALALALA ,  anno ->GIGIGIGIGI, change boss name");
-        assassins.setName("LALALALALLALA");
-        anno.setName("GIGIGIGIGIIGIG");
-
-        ubisoft.setBoss(eaLead);
-        companyRepository.updateCompany(ubisoft);
-
-
-        gameRepository.updateGames(anno);
-        gameRepository.updateGames(assassins);
-
-        System.out.println("All companies");
-        companyRepository.getAllCompany().forEach(company -> System.out.println(company.toString()));
-
-        System.out.println("All games");
-        gameRepository.getAllGames().forEach(game -> System.out.println(game.toString()));
-
-        System.out.println("All bosses");
-        personRepository.getAllPersons().forEach(person -> System.out.println(person.toString()));
-
-
-        System.out.println("/--------------------------------------------------/");
-        System.out.println("Delete game");
-
-        ubisoft.deleteGameFromGamesList(assassins);
-        companyRepository.updateCompany(ubisoft);
-
-        System.out.println("All companies");
-        companyRepository.getAllCompany().forEach(company -> System.out.println(company.toString()));
-
-        System.out.println("All games");
-        gameRepository.getAllGames().forEach(game -> System.out.println(game.toString()));
-
-
-        System.out.println("/--------------------------------------------------/");
-        System.out.println("Delete company");
-        companyRepository.deleteCompany(ubisoft);
-
-        System.out.println("All companies");
-        companyRepository.getAllCompany().forEach(company -> System.out.println(company.toString()));
-
-        System.out.println("All games");
-        gameRepository.getAllGames().forEach(game -> System.out.println(game.toString()));
-
-        System.out.println("All bosses");
-        personRepository.getAllPersons().forEach(person -> System.out.println(person.toString()));
-
+        HibernateConnection.shutDown();
     }
 }
